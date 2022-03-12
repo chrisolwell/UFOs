874c84d1cb526e196f8c2a23e4be770e6fe2535e
Chris Olwell <chrisolwell@Chriss-MacBook-Pro.local>
2022-03-12 17:57:33 -0600

    Built out html and created filter button, but need to add table data.

--- static/js/app.js
+++ static/js/app.js
@@ -18,8 +18,8 @@ var tbody = d3.select("tbody");
 function buildTable(data) {
     //first clear out existing data
     tbody.html("");
-//next loop through each object in the data
-//and append a row and cells for each calue in the row
+    //next loop through each object in the data
+    //and append a row and cells for each calue in the row
     data.forEach((dataRow)=>{
         //append a row to the table body
         let row = tbody.append("tr");
@@ -33,3 +33,29 @@ function buildTable(data) {
         );
     });
 }
+//if we are to listen for a click, we must first create a button to be clicked
+function handleClick(){
+    //Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //check to see if a date was entered and filder the data using that date.
+    //apply a humble filter aka if then statement
+    if (date) {
+        //Apply `filter` to the table data to only keep the 
+        //rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row =>row.datetime===date);
+    };
+
+    //rebuild the table using the filtered data
+    //@NOTE: if no date was enderted, then filterData will
+    //just be the original tableData.
+    buildTable(filteredData);
+};
+
+//attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
+
